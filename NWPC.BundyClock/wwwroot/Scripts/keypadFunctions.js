8cec3b6f50beef3e730c60aa441b9c1642b752c8
hndrzn <lapidhec@gmail.com>
2024-04-09 20:56:11 +0800

    Implemented Autocomplete - April 9

--- NWPC.BundyClock/wwwroot/Scripts/keypadFunctions.js
+++ NWPC.BundyClock/wwwroot/Scripts/keypadFunctions.js
@@ -1,15 +1,23 @@
 function updateInput(value) {
-    var input = document.getElementById('employeeID');
+    var input = document.getElementById('employeeInput');
+    // Handle appending or deleting characters
     if (value === 'delete') {
         input.value = input.value.slice(0, -1); // Delete the last character
     } else {
         input.value += value; // Append the provided value
     }
+
+    // Trigger a change event to simulate user input
+    var event = new Event('input', {
+        bubbles: true,
+        cancelable: false,
+    });
+    input.dispatchEvent(event);
 }
 
 function toggleKeypad() {
-    var numpad = document.querySelector('.numpad-container');
-    var letterKeypad = document.querySelector('.letter-keypad-container');
+    var numpad = document.getElementById('onScreenNumpad');
+    var letterKeypad = document.getElementById('onScreenletterKeypad');
 
     if (numpad.classList.contains('hidden')) {
         // Switching from letter keypad to numpad
@@ -23,10 +31,22 @@ function toggleKeypad() {
 }
 
 function deleteLast() {
-    var input = document.getElementById('employeeID');
+    var input = document.getElementById('employeeInput');
     input.value = input.value.slice(0, -1);
+    // Trigger a change event to simulate user input
+    var event = new Event('input', {
+        bubbles: true,
+        cancelable: true,
+    });
+    input.dispatchEvent(event);
 }
 
 function appendToInput(value) {
-    document.getElementById('employeeID').value += value;
+    document.getElementById('employeeInput').value += value;
+    // Trigger a change event to simulate user input
+    var event = new Event('input', {
+        bubbles: true,
+        cancelable: true,
+    });
+    input.dispatchEvent(event);
 }
